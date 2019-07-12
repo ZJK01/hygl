e470cc1b7efecfbe054b1b3080b03218f9ecb01d
hygl <2374960291@qq.com>
2019-07-12 09:43:35 +0800

    新项目

---
+++ SSH001/src/main/java/com/hxzy/zjk/SSH001/App.java
@@ -0,0 +1,13 @@
+package com.hxzy.zjk.SSH001;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
